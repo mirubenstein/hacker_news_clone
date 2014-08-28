2172a1406dc50954b2ad19b861980e69edcd954d
Andrew Benavides & Mark Rubenstein <student@epicodus.com>
2014-08-27 17:32:40 -0700

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   has_many :links
   validates :password_digest, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
 end
